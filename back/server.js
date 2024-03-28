8ba5e054c7bdab0436b452f4394e956e51cc6fe3
unknown <wassouf.ryan@outlook.fr>
2024-03-28 14:06:29 +0100

    Ajout de fonctionnalité

--- back/server.js
+++ back/server.js
@@ -51,6 +51,8 @@ app.use(cors()); // Permet de gerer les requetes entre serveurs
 //Autoriser toutes les IP
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
